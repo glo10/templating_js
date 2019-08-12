881e9184ba1d9bdbbfae528bf8735f2ebcd17d97
glodie <glodie.tshimini@gmail.com>
2019-08-12 18:07:20 +0200

    Ajout des commentaires

--- js/main.js
+++ js/main.js
@@ -2,18 +2,19 @@ $(document).ready(function() {
   let patternRegex = new RegExp('{{ (.*) }}', 'g')
   let container = $('#container')
   let containerContent = container.html()
+  //data insert to html
   let data = {
     title: 'Titre de niveau 1',
     paragraph: 'ceci est un paragraphe',
     span: 'hello world',
-    hello: 'null',
+    hello: null,
     world:'thank',
-
   }
-
+  // replacement tags html by data
   while ((content = patternRegex.exec(containerContent)) != null) {
     let key = content[0]
     let value = content[1]
+    //empty value for undefined key or null value
     if(data[key] == 'undefined' || !data[value] ){
       containerContent = containerContent.replace(key, '')
       console.error(' data error:',`No key ${key} in data or his value is null`)
